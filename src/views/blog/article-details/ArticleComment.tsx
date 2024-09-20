9babba1da9f5d581bb31e44312bee86344ff7010
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-20 03:09:40 +0100

    feat: populate user in article comment

--- src/views/blog/article-details/ArticleComment.tsx
+++ src/views/blog/article-details/ArticleComment.tsx
@@ -1,35 +1,12 @@
-import React, { useEffect, useState } from 'react';
 import { Stack, Box, Typography, Tooltip, Fab } from '@mui/material';
 import { IconCircle, IconTrash } from '@tabler/icons';
 import { CommentType } from 'src/types/blog';
-import axios from 'src/utils/axios';
-import { UserType } from 'src/types/user';
 import ArticleCommentReply from './ArticleCommentReply';
 import { dispatch } from 'src/store/Store';
 import { deleteSelectedComment } from 'src/store/blog/BlogSlice';
 import { formattedDate } from 'src/utils/usefulFunctions/formattedDate';
 
 const ArticleComment = ({ comment }: CommentType | any) => {
-    const [user, setUser] = useState<UserType>();
-
-    // TODO: correct this later
-    const API_URL = 'http://localhost:5000/api/v1';
-
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const response = await axios.get(`${API_URL}/user/${comment.uid}`);
-                setUser(response.data);
-            } catch (err) {
-                console.error('Error fetching user:', err);
-            }
-        };
-
-        if (comment.uid) {
-            fetchUser();
-        }
-    }, [comment.uid]);
-
     const handleDeleteComment = (id: string) => {
         if (window.confirm('Are you sure you want to delete this comment?')) {
             dispatch(deleteSelectedComment(id));
@@ -40,7 +17,7 @@ const ArticleComment = ({ comment }: CommentType | any) => {
         <>
             <Box mt={2} p={3} sx={{ backgroundColor: 'grey.100' }}>
                 <Stack direction={'row'} gap={2} justifyContent="space-between">
-                    <Typography variant="h6">{user ? user?.name : 'User'}</Typography>
+                    <Typography variant="h6">{comment.user ? comment.user?.name : 'User'}</Typography>
                     <Typography variant="caption" color="textSecondary">
                         <>
                             <IconCircle size="7" fill="" fillOpacity={'0.1'} strokeOpacity="0.1" />{' '}
