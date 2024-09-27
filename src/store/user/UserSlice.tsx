c6a87cc4d6e752c984ff79b93d99170bd0bc4fea
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-27 20:17:26 +0100

    feat: update user model and slice

--- src/store/user/UserSlice.tsx
+++ src/store/user/UserSlice.tsx
@@ -30,41 +30,30 @@ export const UserSlice = createSlice({
     getUser: (state: StateType, action) => {
       state.selectedUser = action.payload;
     },
-    deleteUser: (state: StateType, action) => {
-      const userIndex = state.users.findIndex((user) => user._id === action.payload);
-      if (userIndex !== -1) {
-        state.users[userIndex].deleted = true;
-      }
-    },
-    activateUser: (state: StateType, action) => {
-      const userIndex = state.users.findIndex((user) => user._id === action.payload);
-      if (userIndex !== -1) {
-        state.users[userIndex].deleted = false;
-      }
-    },
   },
 });
 
-export const { getUsers, getUser, deleteUser, activateUser } = UserSlice.actions;
+export const { getUsers, getUser } = UserSlice.actions;
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export const fetchUsers = (role?: string, keywords?: string, size = 10, page = 1, sort = "desc") => async (dispatch: AppDispatch) => {
-  try {
-    const response = await axios.get(`${API_URL}/user`, {
-      params: {
-        role,
-        keywords,
-        size,
-        page,
-        sort
-      },
-    });
-    dispatch(getUsers(response.data));
-  } catch (err) {
-    throw new Error();
-  }
-};
+export const fetchUsers =
+  (keywords?: string, size = 10, page = 1, sort = 'desc') =>
+  async (dispatch: AppDispatch) => {
+    try {
+      const response = await axios.get(`${API_URL}/user`, {
+        params: {
+          keywords,
+          size,
+          page,
+          sort,
+        },
+      });
+      dispatch(getUsers(response.data));
+    } catch (err) {
+      throw new Error();
+    }
+  };
 export const fetchUser = (id: string) => async (dispatch: AppDispatch) => {
   try {
     const response = await axios.get(`${API_URL}/user/${id}`);
@@ -73,20 +62,5 @@ export const fetchUser = (id: string) => async (dispatch: AppDispatch) => {
     throw new Error(err);
   }
 };
-export const deleteUserById = (id: string) => async (dispatch: AppDispatch) => {
-  try {
-    await axios.delete(`${API_URL}/user/${id}`);
-    dispatch(deleteUser(id));
-  } catch (err: any) {
-    throw new Error(err);
-  }
-};
-export const activateUserById = (id: string) => async (dispatch: AppDispatch) => {
-  try {
-    await axios.post(`${API_URL}/user/activate-user/${id}`);
-    dispatch(activateUser(id));
-  } catch (err: any) {
-    throw new Error(err);
-  }
-};
+
 export default UserSlice.reducer;
