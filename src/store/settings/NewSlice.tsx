6b593d3821809efe2eabe8d70a1b56d61d9c7f70
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-10-09 21:34:21 +0100

    feat: new settings model and slice

---
+++ src/store/settings/NewSlice.tsx
@@ -0,0 +1,51 @@
+import axios from 'src/utils/axios';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AppDispatch } from 'src/store/Store';
+import { NewSettingsType } from 'src/types/settings/new';
+
+interface StateType {
+  settings: NewSettingsType;
+}
+
+const initialState: StateType = {
+  settings: {
+    brands: [],
+  },
+};
+
+export const NewSettingsSlice = createSlice({
+  name: 'NewSettings',
+  initialState,
+  reducers: {
+    setNewSettings: (state, action: PayloadAction<any>) => {
+      state.settings = action.payload;
+    },
+  },
+});
+
+export const { setNewSettings } = NewSettingsSlice.actions;
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+// Fetch settings from the API
+export const fetchNewSettings = () => async (dispatch: AppDispatch) => {
+  try {
+    const response = await axios.get(`${API_URL}/settings/new`);
+    dispatch(setNewSettings(response.data));
+  } catch (err) {
+    throw new Error('Failed to fetch settings');
+  }
+};
+
+// Update settings
+export const updateNewSettings =
+  (newNewSettings: NewSettingsType) => async (dispatch: AppDispatch) => {
+    try {
+      const response = await axios.put(`${API_URL}/settings/new`, newNewSettings);
+      dispatch(setNewSettings(response.data));
+    } catch (err) {
+      throw new Error('Failed to update settinfs');
+    }
+  };
+
+export default NewSettingsSlice.reducer;
